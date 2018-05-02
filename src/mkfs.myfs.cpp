09a11240abec9fc79bd0de6ee5fd4ee86c99b296
Milan Kostić <allgemein-m.kostic@gmx.de>
2018-05-02 16:15:54 +0200

    Quelltext aus Übung 2 (02.05.2018)

--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -11,8 +11,24 @@
 #include "macros.h"
 
 int main(int argc, char *argv[]) {
-
+    int errorNumber = 0;
+    int filecount = argc -1;
     // TODO: Implement file system generation & copying of files here
+    if(argc > 0) {
+        errorNumber = BlockDevice::create(argv[0]);
+        if (errorNumber < 0) {
+            error("mkfs.myfs containerdatei [input-datei...]\");");
+            return errorNumber;
+        }
+    }
     
+    if (filecount > 0) {
+        while (filecount > 0) {
+            //Datei erstekken
+            //Datei lesen
+            //Datein schreiben
+            filecount--;
+        }
+    }
     return 0;
 }
